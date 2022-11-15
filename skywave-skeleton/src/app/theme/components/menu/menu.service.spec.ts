f48b03570d0c06a0502667464f90fea5156d8697
AravindPavanKalyan <aravindpavankalyan1996@gmail.com>
2022-11-15 13:57:03 +0530

    menu service spec

--- skywave-skeleton/src/app/theme/components/menu/menu.service.spec.ts
+++ skywave-skeleton/src/app/theme/components/menu/menu.service.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { Menu } from './menu.model';
 import { MenuService } from './menu.service';
 
 describe('MenuService', () => {
@@ -20,5 +21,16 @@ describe('MenuService', () => {
   // testing whether MenuService is created
   it('should create MenuService', () => {
     expect(service).toBeTruthy();
+  });
+
+  // testing getHorizontalMenuItems
+  it('should create getHorizontalMenuItems', () => {
+    const mockHorizontalMenuItems = [
+      new Menu(1, 'NAV.HOME', '/', null, null, false, 0),
+      new Menu(80, 'NAV.ABOUT_US', '/about', null, null, false, 0),
+      new Menu(70, 'NAV.CONTACT', '/contact', null, null, false, 0),
+    ];
+    const horizontalMenuItems = service.getHorizontalMenuItems();
+    expect(horizontalMenuItems).toEqual(mockHorizontalMenuItems);
   });
 });
